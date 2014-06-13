19073f6c16adc97afd2eacfcd766749b644594b1
Kris Jenkins <krisajenkins@gmail.com>
2014-06-13 10:15:46 +0100

    Various improvements.

--- Main.hs
+++ Main.hs
@@ -1,54 +1,80 @@
 module Main where
-
+import           Control.Applicative
 import qualified Data.Map as Map
+import           Data.Monoid
 import           System.Random
-import Data.Monoid
-import Control.Applicative
 
-data Node a = Node a | Start | End deriving (Eq, Ord, Show)
-type Graph a = Map.Map (Node a) [Node a]
+------------------------------------------------------------
+-- General functions.
+------------------------------------------------------------
 
-oneOf :: RandomGen g => [a] -> g -> (a,g)
-oneOf xs g = ((!!) xs n, g')
-  where (n,g') = randomR (0, length xs - 1) g
-
+-- |Adds items to the beginning & end of a list.
 addHeadAndTail :: a -> a -> [a] -> [a]
-addHeadAndTail h t xs = [h] ++ xs ++ [t]
+addHeadAndTail h t xs = [h] <> xs <> [t]
+
+oneOf :: RandomGen r => ([a], r) -> Maybe (a,r)
+oneOf ([], _) = Nothing
+oneOf (xs, r) = Just ((!!) xs n, r')
+  where (n,r') = randomR (0, length xs - 1) r
 
 pairs :: [a] -> [(a,a)]
-pairs [] = []
-pairs [_] = []
-pairs [x,y] = [(x,y)]
-pairs (x:y:xs) = (x,y) : pairs(y:xs)
+pairs xs = zip xs (tail xs)
 
-addPair :: Ord a => Map.Map a [b] -> (a,b) -> Map.Map a [b]
+------------------------------------------------------------
+-- Types.
+------------------------------------------------------------
+
+-- data Node a = Node a | Start | End deriving (Eq, Ord, Show)
+data Node a = Node a deriving (Eq, Ord, Show)
+data From a = From (Node a) | Start deriving (Eq, Ord, Show)
+data To a = To (Node a) | End deriving (Eq, Ord, Show)
+type Edge a = (From a, To a)
+type Graph a = Map.Map (From a) [To a]
+
+------------------------------------------------------------
+-- Build the chains.
+------------------------------------------------------------
+
+edges :: [Node a] -> [Edge a]
+edges xs = zip (fmap From xs) (fmap To (tail xs))
+
+addPair :: Ord a => Graph a -> Edge a -> Graph a
 addPair t (x,y) = Map.insert x (y:Map.findWithDefault [] x t) t
 
 addChain :: Ord a => Graph a -> [Node a] -> Graph a
-addChain t xs = foldl addPair t (pairs xs)
-
---
-
-readChainStep :: (Ord a, RandomGen g) => Graph a -> (Node a, g) -> [a]
-readChainStep _ (End, _) = []
-readChainStep t (Node w, g) =
-  case Map.lookup (Node w) t of
-       Nothing -> []
-       Just xs -> w:readChainStep t (oneOf xs g)
-
-readChain :: (Ord a, RandomGen g) => Graph a -> g -> [a]
-readChain t g =
-  readChainStep t (case Map.lookup Start t of
-                     Nothing -> (End, g)
-                     Just ws -> oneOf ws g)
+addChain t xs = foldl addPair t . addHeadAndTail (Start, To (head xs)) (From (last xs), End) $ edges xs
 
 lineToSentence :: String -> [Node String]
-lineToSentence = addHeadAndTail Start End . fmap Node . words
+lineToSentence = fmap Node . words
+
+------------------------------------------------------------
+-- Read the chains.
+------------------------------------------------------------
+
+anyTo :: (RandomGen r, Ord a) => Graph a -> (From a, r) -> Maybe (To a, r)
+anyTo g (f, r) = do
+                  ts <- Map.lookup f g
+                  oneOf (ts, r)
+
+asList :: From a -> [a]
+asList Start = []
+asList (From (Node a)) = [a]
+
+readChain :: (Ord a, RandomGen r) => Graph a -> From a -> r -> [a]
+readChain g f r =
+  case anyTo g (f, r) of
+       Just (To a, r') -> asList f `mappend` readChain g (From a) r'
+       Just (End, _) -> asList f
+       Nothing -> asList f
+
+------------------------------------------------------------
+-- Main
+------------------------------------------------------------
 
 main :: IO ()
 main = do
+  r <- getStdGen
   text <- readFile "corpus.txt"
-  let sentences = fmap lineToSentence $ lines text
+  let sentences = lineToSentence <$> lines text
       tree = foldl addChain Map.empty sentences
-  g <- getStdGen
-  print $ unwords $ readChain tree g
+  print . unwords $ readChain tree Start r
